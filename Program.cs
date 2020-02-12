dfa0ec2e2fa3c0a369e68df35c69dd89c719a2af
Brendan Krekeler <bfkyb4@mail.missouri.edu>
2020-02-12 14:19:50 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Document
+{
+    class start
+    {
+        public static void Greeting()
+        {
+            Console.WriteLine("----Document----\n");
+            Console.ReadLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start.Greeting(); 
+        }
+    }
+}
+
