169d8a65414b41cbeb1638c9b58a238baeddf947
Brendan Krekeler <36323683+okrek6@users.noreply.github.com>
2020-02-13 18:51:32 -0600

    Final commit on working

--- Program.cs
+++ Program.cs
@@ -3,23 +3,36 @@ using System.IO;
 
 namespace Document
 {
-    class start
+    class Document
     {
-        public static string CreateDoc()
+        public static string CreateDoc(string fileName, string text)
         {
-            Console.WriteLine("----Document----\n");
-            Console.Write("Enter the name of the Document: ");
-            string fileName = Console.ReadLine();
-            Console.Write("Type what is in the Document: ");
-            string text = Console.ReadLine();
-            return text;
+            try
+            {
+                StreamWriter streamWriter = new StreamWriter(fileName + ".txt");
+                streamWriter.WriteLine(text);
+                streamWriter.Close();
+                string success = ($"{fileName} was saved. The doc has {text.Length} characters in it.");
+                return success;
+            }
+            catch (Exception e) //this passes in a variable e of type Exception to be used in the scope
+            {
+                string fail = ("Exception: " + e.Message);
+                return fail;
+            }
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            start.CreateDoc(); 
+            Console.WriteLine("----Document----\n");
+            Console.Write("Enter the name of the Document: ");
+            string fileName = Console.ReadLine();
+            Console.Write("Type what is in the Document: ");
+            string text = Console.ReadLine();
+            string result = Document.CreateDoc(fileName, text);
+            Console.WriteLine(result); 
         }
     }
 }
